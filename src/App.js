74f15dbb56ab2dc69b2e5ebe6fa5e0763f43ec62
jeongwookkim <mybiggold@naver.com>
2025-01-03 22:19:27 -0800

    [feat]프로젝트 시작

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="log">
+      😱 Jeongwook | running | Elapsed Time: 2.00 seconds
+      </div>
+    </div>
+  );
+}
+
+export default App;
