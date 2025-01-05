3a135d386f6bda1d77a41f86d0b66a5fd4df96cd
ddongni <de0309@naver.com>
2025-01-05 13:48:05 -0800

    configured deployment

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,7 @@ module.exports = {
     },
     devtool: false,
     devServer: {
-        host: '0.0.0.0',
-        allowedHosts: 'all'
+        disableHostCheck: true
     },
     resolve: {
         symlinks: true
