937f428d09b7d21af42c011fd6ec29504d7c967d
ddongni <de0309@naver.com>
2025-01-05 18:45:58 -0800

    fixed auth

--- src/app/login/LoginSuccess.js
+++ src/app/login/LoginSuccess.js
@@ -11,21 +11,34 @@ const LoginSuccess = () => {
     const params = new URLSearchParams(location.search);
     const nickname = params.get('nickname');
 
+    const getEmailFromCookies = async () => {
+        return new Promise((resolve, reject) => {
+            try {
+                const value = `; ${document.cookie}`;
+                const parts = value.split(`; email=`);
+                if (parts.length === 2) {
+                    const email = parts.pop().split(';').shift();
+                    resolve(email); // 이메일 반환
+                } else {
+                    resolve(null); // 이메일이 없을 경우 null 반환
+                }
+            } catch (error) {
+                reject(error); // 에러 발생 시 reject
+            }
+        });
+    };
+
     useEffect(() => {
        const updateNicknameAndNavigate = async () => {
             if (nickname && nickname.length > 0) {
                 try {
                     await dispatch(setNickname(nickname));
 
-                    const value = `; ${document.cookie}`;
-                    const parts = value.split(`; email=`);
-                    if (parts.length === 2) {
-                        const email = parts.pop().split(';').shift();
-                        if (email) {
-                            localStorage.setItem("email", email);
-                        }
+                    const email = await getEmailFromCookies();
+                    if (email) {
+                        localStorage.setItem("email", email);
                     }
-
+                    
                     navigate('/');
                 } catch (error) {
                     console.error('Nickname update failed:', error);
